801295b4adaf664178905cea9bc771ca655d44f7
최현준 <choijjunii1@gmail.com>
2025-03-04 15:18:57 +0900

    express로 서버열기

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const a = "Hello,World!"
-console.log(a);
+//todo express와 express-handlebars 설치완료 후 require로 요청받기// $ npm install express-handlebars //
+//require 함수로 모듈가져오기 (commonJS)
+const express = require('express');
+const app = express();
+//PORT 3000 설정
+const PORT = 3000;
+
+
+
+app.listen(PORT, () => {
+  console.log('Loading...','http://localhost:3000')
+});
